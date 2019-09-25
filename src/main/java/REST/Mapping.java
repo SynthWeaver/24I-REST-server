1150365fd3412f31be0f5728b4e55577f621aec1
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2019-09-25 16:21:34 +0200

    Works with sams code v0.6

--- src/main/java/REST/Mapping.java
+++ src/main/java/REST/Mapping.java
@@ -4,13 +4,10 @@ import database.DB;
 import objects.DateTime;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class Mapping {
@@ -34,19 +31,12 @@ public class Mapping {
     }
 
     @ResponseBody
-    @PostMapping("/post")
-    public void post(HttpServletRequest request){
-        String smiley = request.getParameter("smiley");
-        String feedback = request.getParameter("feedback");
-        String device = request.getParameter("device");
-
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("smiley", smiley);
-        jsonObject.put("feedback", feedback);
-        jsonObject.put("device", device);
+    @RequestMapping(value = "/post", method = RequestMethod.POST, consumes = "text/plain")
+    public void post(@RequestBody String json) throws ParseException {
+        JSONObject jsonObject = (JSONObject) new JSONParser().parse(json);
+        jsonObject.put("device", "Apple Iphone 4");
         jsonObject.put("time", DateTime.now());
-
+        
         db.insert(jsonObject);
     }
-
 }
