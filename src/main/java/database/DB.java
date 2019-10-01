95e7d4ee0c79683934c8ea7bead188c5cdf68743
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2019-10-01 14:38:18 +0200

    db test get

--- src/main/java/database/DB.java
+++ src/main/java/database/DB.java
@@ -1,5 +1,6 @@
 package database;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -8,38 +9,45 @@ import org.json.simple.parser.ParseException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DB {
 
-    public void insert(JSONObject jsonObject){
-        //get old data
-        JSONArray jsonArray = this.selectAll();
+    private MysqlDataSource dataSource = new MysqlDataSource();
+    private Connection conn;
+    private Statement stmt;
+    ResultSet rs;
 
-        //add new data to old data
-        jsonArray.add(jsonObject);
-
-        //write to local data
-        try (FileWriter file = new FileWriter("DB.json")) {
-            file.write(jsonArray.toJSONString());
-            file.flush();
-            System.out.println("Added");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public DB(){
+        dataSource.setUser("root");
+        dataSource.setPassword("my24I");
+        dataSource.setServerName("myDBHost.example.org");
     }
 
-    public JSONArray selectAll(){
-        //JSON parser object to parse read file
-        JSONParser jsonParser = new JSONParser();
-
-        try (FileReader reader = new FileReader("DB.json"))
-        {
-            //Read JSON file
-            return (JSONArray) jsonParser.parse(reader);
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
-        return null;
+    private void open() throws SQLException {
+        conn = dataSource.getConnection();
+        stmt = conn.createStatement();
     }
+
+    private void close() throws SQLException {
+        rs.close();
+        stmt.close();
+        conn.close();
+    }
+
+    public String selectAll() throws SQLException {
+        open();
+        String query = "SELECT * FROM entry";
+        rs = stmt.executeQuery(query);
+        String result = rs.toString();
+        close();
+        return result;
+    }
+
+    
+
 
 }
