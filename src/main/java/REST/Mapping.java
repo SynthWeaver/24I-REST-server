94148313aa9b237b0e67407b0d2634d08a21f062
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2019-10-02 14:38:31 +0200

    testing images

--- src/main/java/REST/Mapping.java
+++ src/main/java/REST/Mapping.java
@@ -1,13 +1,14 @@
 package REST;
 
 import database.DB;
-import objects.DateTime;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.SQLException;
 
@@ -23,10 +24,17 @@ public class Mapping {
     }
 
     @ResponseBody
-    @RequestMapping(value = "/post", method = RequestMethod.POST, consumes = "text/plain")
+    @RequestMapping(value = "/test", method = RequestMethod.POST, consumes = "text/plain")
     public void post(@RequestBody String json) throws ParseException, SQLException {
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject = (JSONObject) jsonParser.parse(json);
        db.insert(jsonObject);
+    }
+
+    @PostMapping("/post")
+    public String handleFileUpload(@RequestParam("image") MultipartFile image,
+                                   RedirectAttributes redirectAttributes) {
+
+        return "redirect:/get";
     }
 }
