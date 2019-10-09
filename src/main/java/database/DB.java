79c4a3ea1556072013bb78ac81b1501db03ddd8f
HR <oheru002@edu.xamk.fi>
2019-10-09 11:28:54 +0200

    Added DateTime.java
    For correct DateTime form for SQL

--- src/main/java/database/DB.java
+++ src/main/java/database/DB.java
@@ -1,10 +1,9 @@
 package database;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import nl.terwijn.DateTime;
+import objects.DateTime;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-
 import java.sql.*;
 
 public class DB {
